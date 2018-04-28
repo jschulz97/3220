fec3150e0df68b7df7082ed62e06e4eca8631e71
akirk77 <andrewkirkham100@yahoo.com>
2018-04-28 16:06:43 -0500

    Add files via upload

---
+++ FinalProjectv1.cpp
@@ -0,0 +1,330 @@
+//============================================================================
+// Name        : FinalProjectv1.cpp
+// Author      : Andrew
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include<iostream>
+#include<fstream>
+#include<cctype>
+#include<iomanip>
+using namespace std;
+/**************************************************************************************************/
+class account
+{
+	int act_num;
+	int dep;
+	char act_name[50];
+	char act_type;
+public:
+	void display_account() const;
+	void create_account();
+	void edit();
+	void report() const;
+	void deposit(int);
+	void withdraw(int);
+	char rettype() const;
+	int retact_num() const;
+	int retdeposit() const;
+};
+
+void account::display_account() const
+{
+	cout << endl << "Account Number: " << act_num;
+	cout << endl << "Account Username: " << act_name;
+	cout << endl << "Type of Account: " << act_type;
+	cout << endl << "Balance: " << dep;
+}
+
+void account::create_account()
+{
+	cout << endl << "Enter Account Number: ";
+	cin >> act_num;
+	cout << endl << endl << "Enter Name: ";
+	cin.ignore();
+	cin.getline(act_name,50);
+	cout << endl << "What Type of Account do you want to open? (C for Checkings, S for Savings): ";
+	cin >> act_type;
+	act_type=toupper(act_type);
+	cout << endl << "Enter Initial Amount: ";
+	cin >> dep;
+	cout << endl << "Account Created";
+}
+
+void account::edit()
+{
+	cout << endl << "Account Number: " << act_num;
+	cout << endl << "Edit Account Username: ";
+	cin.ignore();
+	cin.getline(act_name,50);
+	cout << endl << "Edit Type of Account: ";
+	cin >> act_type;
+	act_type = toupper(act_type);
+	cout << endl << "Edit Balance amount : ";
+	cin >> dep;
+}
+
+void account::report() const
+{
+	cout << act_num << setw(10) << " " << act_name << setw(10) << " " << act_type << setw(6) << dep << endl;
+}
+
+void account::deposit(int x)
+{
+	dep += x;
+}
+
+void account::withdraw(int x)
+{
+	dep -= x;
+}
+
+char account::rettype() const
+{
+	return act_type;
+}
+
+int account::retact_num() const
+{
+	return act_num;
+}
+
+int account::retdeposit() const
+{
+	return dep;
+}
+
+/****************************************************************************************************/
+
+void delete_account(int);
+void deposit_withdraw(int, int);
+void display_account(int);
+void display_all();
+void edit_account(int);
+void write_account();
+
+int main()
+{
+	char choice;
+	int num;
+	do
+	{
+		system("cls");
+		cout << endl << "\t!!!Welcome to S&K Bank!!!";
+		cout << endl << endl << "1) New Account";
+		cout << endl << endl << "2) Deposit";
+		cout << endl << endl << "3) Withdraw";
+		cout << endl << endl << "4) Balance";
+		cout << endl << endl << "5) Display All Accounts";
+		cout << endl << endl << "6) Close an Account";
+		cout << endl << endl << "7) Edit the Details of an Account";
+		cout << endl << endl << "8) Exit";
+		cout << endl << endl << "Make Your Choice (1-8) and don't be a smartass and pick something besides these";
+		cin >> choice;
+		system("cls");
+		switch(choice)
+		{
+		case '1':
+			write_account();
+			break;
+		case '2':
+			cout << endl << endl << "Enter Account Number: ";
+			cin >> num;
+			deposit_withdraw(num, 1);
+			break;
+		case '3':
+			cout << endl << endl << "Enter Account Number: ";
+			cin >> num;
+			deposit_withdraw(num, 2);
+			break;
+		case '4':
+			cout << endl << endl << "Enter Account Number: ";
+			cin>>num;
+			display_account(num);
+			break;
+		case '5':
+			display_all();
+			break;
+		case '6':
+			cout << endl << endl << "Enter Account Number: ";
+			cin>>num;
+			delete_account(num);
+			break;
+		 case '7':
+			cout << endl << endl << "Enter Account Number: ";
+			cin>>num;
+			edit_account(num);
+			break;
+		 case '8':
+			cout << endl << endl << "Thanks Homie";
+			break;
+		 default:
+			 cout << "\a";
+		}
+		cin.ignore();
+		cin.get();
+	}
+	while(choice != '8');
+	return 0;
+}
+
+void delete_account(int n) //delete an account
+{
+	account acc;
+	ifstream inFile;
+	ofstream outFile;
+	inFile.open("account.dat",ios::binary);
+	if(!inFile)
+	{
+		cout << "Something went wrong, the File couldn't be opened. Press any key to continue";
+		return;
+	}
+	outFile.open("Temp.dat",ios::binary);
+	inFile.seekg(0,ios::beg);
+	while(inFile.read(reinterpret_cast<char *> (&acc), sizeof(account)))
+	{
+		if(acc.retact_num() != n)
+		{
+			outFile.write(reinterpret_cast<char *> (&acc), sizeof(account));
+		}
+	}
+	inFile.close();
+	outFile.close();
+	remove("account.dat");
+	rename("Temp.dat", "account.dat");
+	cout << endl << endl << "\tAccount Deleted";
+}
+
+void deposit_withdraw(int x, int choice) //figured I'd put deposit and withdraw in the same function for simplicities sake
+{
+	int amt;
+	bool b = false;
+	account acc;
+	fstream File;
+	File.open("account.dat", ios::binary|ios::in|ios::out);
+	if(!File)
+	{
+		cout << "Something went wrong, the file couldn't be opened. Press any key to continue";
+		return;
+	}
+	while(!File.eof() && b == false)
+	{
+		File.read(reinterpret_cast<char *> (&acc), sizeof(account));
+		if(acc.retact_num() == x)
+		{
+			acc.display_account();
+			if(choice == 1)
+			{
+				cout << endl << endl << "\tDepositing";
+				cout << endl << endl << "Enter Amount to be Deposited";
+				cin >> amt;
+				acc.deposit(amt);
+			}
+			if(choice == 2)
+			{
+				cout << endl << endl << "\tWithdrawing";
+				cout << endl << endl << "Enter Amount to be Withdrawn";
+				cin >> amt;
+				int bal = acc.retdeposit() - amt;
+				if((bal < 0 && acc.rettype() == 'S') || (bal < 0 && acc.rettype() == 'C'))
+					cout << "Insufficience Balance";
+				else
+					acc.withdraw(amt);
+			}
+			int pos = (-1)*static_cast<int>(sizeof(acc));
+			File.seekp(pos, ios::cur);
+			File.write(reinterpret_cast<char *> (&acc), sizeof(account));
+			cout << endl << endl << "\tAccount Updated";
+			b = true;
+	       }
+         }
+	File.close();
+	if(b == false)
+		cout << endl << endl << "We Couldn't Find the Record, Please Try Again";
+}
+
+void display_account(int x) //read record from file
+{
+	account acc;
+	bool b = false;
+	ifstream inFile;
+	inFile.open("account.dat",ios::binary);
+	if(!inFile)
+	{
+		cout << "Something went wrong, the File couldn't be opened. Press any key to continue";
+		return;
+	}
+	cout << endl << "Balance" << endl;
+    while(inFile.read(reinterpret_cast<char *> (&acc), sizeof(account)))
+	{
+		if(acc.retact_num() == x)
+		{
+			acc.display_account();
+			b = true;
+		}
+	}
+	inFile.close();
+	if(b == false)
+	cout << endl << endl << "That Account Number does not exist";
+}
+
+void display_all() //allows the user to see all their accounts
+{
+	account acc;
+	ifstream inFile;
+	inFile.open("account.dat",ios::binary);
+	if(!inFile)
+	{
+		cout << "Something went wrong, the File couldn't be opened. Press any key to continue";
+		return;
+	}
+	cout << endl << endl << "\t\tAccount List" << endl << endl;
+	cout << "Account Number      NAME           Type  Balance" << endl << endl;
+	while(inFile.read(reinterpret_cast<char *> (&acc), sizeof(account)))
+	{
+		acc.report();
+	}
+	inFile.close();
+}
+
+void edit_account(int x) //used to edit a record
+{
+	bool b = false;
+	account acc;
+	fstream File;
+	File.open("account.dat",ios::binary|ios::in|ios::out);
+	if(!File)
+	{
+		cout << "Something went wrong, the File couldn't be opened. Press any key to continue";
+		return;
+	}
+	while(!File.eof() && b == false)
+	{
+		File.read(reinterpret_cast<char *> (&acc), sizeof(account));
+		if(acc.retact_num() == x)
+		{
+			acc.display_account();
+			cout << endl << endl << "Enter New Account Info" << endl;
+			acc.edit();
+			int pos = (-1)*static_cast<int>(sizeof(account));
+			File.seekp(pos,ios::cur);
+			File.write(reinterpret_cast<char *> (&acc), sizeof(account));
+			cout << endl << endl << "\t Account Updated";
+			b = true;
+		  }
+	}
+	File.close();
+	if(b == false)
+		cout<< endl << endl << "We Couldn't Find the Record, Please Try Again";
+}
+
+void write_account() //write to file
+{
+	account acc;
+	ofstream outFile;
+	outFile.open("account.dat",ios::binary|ios::app);
+	acc.create_account();
+	outFile.write(reinterpret_cast<char *> (&acc), sizeof(account));
+	outFile.close();
+}
